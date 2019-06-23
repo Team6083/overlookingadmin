08d1ccb8f00014b30e29bfd01151959fc8630f97
KennHuang <j20281@my.cmsh.cyc.edu.tw>
2019-06-23 13:08:25 +0800

    [modify] change SignUp page to bootstrap

--- src/composment/auth/Signup.js
+++ src/composment/auth/Signup.js
@@ -25,25 +25,26 @@ class Signup extends Component {
         return (
             <div className="container">
                 <form onSubmit={this.handleSubmit} className="white">
-                    <h5 className="grey-text text-darken-3">Sign Up</h5>
-                    <div className="input-field">
+                    <h3>Sign Up</h3>
+
+                    <div className="form-group">
                         <label htmlFor="firstName">First Name</label>
-                        <input type="text" id="firstName" onChange={this.handleChange} />
+                        <input className="form-control" type="text" id="firstName" onChange={this.handleChange} />
                     </div>
-                    <div className="input-field">
+                    <div className="form-group">
                         <label htmlFor="lastName">Last Name</label>
-                        <input type="text" id="lastName" onChange={this.handleChange} />
+                        <input className="form-control" type="text" id="lastName" onChange={this.handleChange} />
                     </div>
-                    <div className="input-field">
+                    <div className="form-group">
                         <label htmlFor="email">Email</label>
-                        <input type="email" id="email" onChange={this.handleChange} />
+                        <input className="form-control" type="email" id="email" onChange={this.handleChange} />
                     </div>
-                    <div className="input-field">
+                    <div className="form-group">
                         <label htmlFor="password">Password</label>
-                        <input type="password" id="password" onChange={this.handleChange} />
+                        <input className="form-control" type="password" id="password" onChange={this.handleChange} />
                     </div>
-                    <div className="input-field">
-                        <button className="btn pink lighten-1 z-depth-0">Sign Up</button>
+                    <div className="form-group">
+                        <button className="btn btn-primary btn-block">Sign Up</button>
                     </div>
                 </form>
             </div>
