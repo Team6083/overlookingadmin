24eeca911d7c2002fdb484918cafe6d8f4f03232
KennHuang <j20281@my.cmsh.cyc.edu.tw>
2019-09-17 09:00:33 +0800

    [modify] Update user roles

--- src/constant/userRoles.js
+++ src/constant/userRoles.js
@@ -1,9 +1,11 @@
 const roles = {
-    UnAuth: "UA",
-    Student: "S",
-    Leader: "L",
+    SystemAdmin: "SA",
     Admin: "A",
-    Teacher: "T"
+    Teacher: "T",
+    Leader: "L",
+    Member: "M",
+    Guest: "G",
+    UnAuth: "UA"
 }
 
 export default roles
