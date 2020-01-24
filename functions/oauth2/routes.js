4166733164721a3923bf96ccc5c6b65a058dd2ae
KennHuang <j20281@my.cmsh.cyc.edu.tw>
2020-01-25 01:24:42 +0800

    [add] Add getUserIdToken to API endpoint

--- functions/oauth2/routes.js
+++ functions/oauth2/routes.js
@@ -1,5 +1,7 @@
 const oauthMiddlewares = require('./oAuthMiddleWare');
 const { createClient } = require('./controllers');
+const admin = require('firebase-admin');
+const { getAuthorizationCode, getUserByID } = require('./model');
 
 function initialize(app) {
   app.all('/oauth/token', oauthMiddlewares.token);
@@ -9,6 +11,16 @@ function initialize(app) {
 
   app.get('/secure', oauthMiddlewares.authenticate, (req, res) => {
     res.json({ message: 'Secure data' });
+  });
+
+  app.get('/getUserIdToken', oauthMiddlewares.authenticate, (req, res) => {
+    if (req.user) {
+      getUserByID(req.user.user).then((user) => {
+        admin.auth().createCustomToken(user._id, { test: true }).then((customToken) => {
+          res.json(customToken);
+        })
+      })
+    }
   });
 
   app.post('/client', createClient);
